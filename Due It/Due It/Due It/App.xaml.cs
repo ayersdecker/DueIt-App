bcaf52e8f0fe732c3fd38bd450d78ba4a571868e
Decker Ayers <ayersdecker@gmail.com>
2022-11-09 15:30:34 -0500

    Initial Setup

---
+++ Due It/Due It/Due It/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Due_It
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new TodayPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Due It/Due It/Due It/PreferencesPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Due_It
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PreferencesPage : ContentPage
+    {
+        public PreferencesPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
