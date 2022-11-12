57c692091515434d268bca5336ae7c7e4c740d6e
Decker Ayers <ayersdecker@gmail.com>
2022-11-12 12:03:07 -0500

    All good

---
+++ Due It/Due It/Due It/Block.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+// Class: Block
+// Description: Used to Block off sections of time within Schedule OBJ 
+//
+namespace Due_It
+{
+    internal class Block
+    {
+
+        
+        // Holds different time periods selectable to block off different times for --> Used for repeatPeriod/RepeatPeriod
+        string[] periods = new string[] {"Never", "Daily", "Weekly", "Monthly"};
+
+        //Stores the name of the Block
+        private string name;
+        //Stores general description given to Block
+        private string description;
+        //Stores selected weekday via Enum --> Sunday = 0, Monday = 1 ...
+        private DayOfWeek dayOfWeek;
+        //Stores selected time of day
+        private TimeSlot timeOfDayStart;
+        private TimeSlot timeOfDayEnd;
+        private string repeatPeriod;
+        public string RepeatPeriod
+        {
+            get { return repeatPeriod; }
+            set
+            {
+                for (int i = 0; i < periods.Length; i++){ if (value == periods[i]){repeatPeriod = value;}}
+                if (repeatPeriod != value){ repeatPeriod = periods[0]; }
+            }
+        }
+    }
+}
---
+++ Due It/Due It/Due It/TimeSlot.cs
@@ -0,0 +1,111 @@
+﻿
+
+namespace Due_It
+{
+    //
+    // This ENUM is meant to signify different fifteen minute intervals throughout the day according to military time.
+    //
+    // Key: ...Zero = 0:00  || ...Fift = 0:15  || ...Thir = 0:30  || ...Fort = 0:45 
+    //
+    //
+    public enum TimeSlot
+    {
+        ZeroFift = 0,       // 0:15 = 0 
+        ZeroThir,
+        ZeroFort,
+        OneZero,            // 1 = 3
+        OneFift,
+        OneThir,
+        OneFort,
+        TwoZero,            // 2 = 7
+        TwoFift,
+        TwoThir,
+        TwoFort,
+        ThreeZero,          // 3 = 11
+        ThreeFift,
+        ThreeThir,
+        ThreeFort,
+        FourZero,           // 4 = 15
+        FourFift,
+        FourThir,
+        FourFort,
+        FiveZero,           // 5 = 19
+        FiveFift,
+        FiveThir,
+        FiveFort,
+        SixZero,            // 6 = 23
+        SixFift,
+        SixThir,
+        SixFort,
+        SevenZero,          // 7 = 27
+        SevenFift,
+        SevenThir,
+        SevenFort,
+        EightZero,          // 8 = 31
+        EightFift,
+        EightThir,
+        EightFort,
+        NineZero,           // 9 = 35
+        NineFift,
+        NineThir,
+        NineFort,
+        TenZero,            // 10 = 39
+        TenFift,
+        TenThir,
+        TenFort,
+        ElevenZero,         // 11 = 43
+        ElevenFift,
+        ElevenThir,
+        ElevenFort,
+        TweleveZero,        // 12-Noon = 47
+        TweleveFift,
+        TweleveThir,
+        TweleveFort,
+        ThirteenZero,       // 13 = 51
+        ThirteenFift,
+        ThirteenThir,
+        ThirteenFort,
+        FourteenZero,       // 14 = 55
+        FourteenFift,
+        FourteenThir,
+        FourteenFort,
+        FifteenZero,        // 15 = 59
+        FifteenFift,
+        FifteenThir,
+        FifteenFort,
+        SixteenZero,        // 16 = 63
+        SixteenFift,
+        SixteenThir,
+        SixteenFort,
+        SeventeenZero,      // 17 = 67
+        SeventeenFift,
+        SeventeenThir,
+        SeventeenFort,
+        EighteenZero,       // 18 = 71
+        EighteenFift,
+        EighteenThir,
+        EighteenFort,
+        NineteenZero,       // 19 = 75
+        NineteenFift,
+        NineteenThir,
+        NineteenFort,
+        TwentyZero,         // 20 = 79
+        TwentyFift,
+        TwentyThir,
+        TwentyFort,
+        TwentyOneZero,      // 21 = 83
+        TwentyOneFift,
+        TwentyOneThir,
+        TwentyOneFort,
+        TwentyTwoZero,      // 22 = 87
+        TwentyTwoFift,
+        TwentyTwoThir,
+        TwentyTwoFort,
+        TwentyThreeZero,    // 23 = 91
+        TwentyThreeFift,
+        TwentyThreeThir,
+        TwentyThreeFort,
+        TwentyFourZero      // 24-Midnight = 95
+
+    }
+}
