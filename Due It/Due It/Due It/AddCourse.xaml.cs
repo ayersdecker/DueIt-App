4d33eef44084720e2cbb51274015dcadf0d4b652
Decker Ayers <ayersdecker@gmail.com>
2023-01-13 05:29:35 -0500

    Monads Add plus add classes UI

--- Due It/Due It/Due It/AddCourse.xaml.cs
+++ Due It/Due It/Due It/AddCourse.xaml.cs
@@ -12,10 +12,16 @@ namespace Due_It
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class AddCourse : ContentPage
     {
+        public List<string> dayOptions = new List<string>
+        {
+            "Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"
+        };
         public AddCourse()
         {
             NavigationPage.SetHasNavigationBar(this, false);
             InitializeComponent();
+            SetMainVisibility(true);
+            dayPicker.ItemsSource= dayOptions;
         }
 
         private void BackButton_Clicked(object sender, EventArgs e)
@@ -25,12 +31,72 @@ namespace Due_It
 
         private void AddClassButton_Clicked(object sender, EventArgs e)
         {
-
+            SetMainVisibility(false);
         }
 
         private void RemoveClassButton_Clicked(object sender, EventArgs e)
         {
 
+        }
+        private void SetMainVisibility(bool visibility)
+        {
+            if(visibility)
+            {
+                but1.Text = "Add Course";
+                but2.IsVisible = true;
+                but3.IsVisible = true; 
+                but4.IsVisible = true;
+                but5.IsVisible = true; 
+                but6.IsVisible = true;
+                but7.IsVisible = true;
+                BackButton.IsVisible = true;
+                NameEntry.IsVisible = true;
+                DescriptionEntry.IsVisible = true;
+                CodeEntry.IsVisible = true;
+                SectionEntry.IsVisible = true;
+                ProfessorEntry.IsVisible = true;
+                AddedClasses.IsVisible = true;
+                AddClassButton.IsVisible = true;
+                RemoveClassButton.IsVisible = true;
+                ClearButton.IsVisible = true;
+                CreateButton.IsVisible = true;
+                add1.IsVisible = false;
+                addStartEntry.IsVisible = false;
+                add2.IsVisible = false;
+                addEndEntry.IsVisible = false;
+                add3.IsVisible = false;
+                dayPicker.IsVisible = false;
+                addClearButton.IsVisible = false;
+                addCreateButton.IsVisible = false;
+            }
+            else
+            {
+                but1.Text = "Add Class";
+                but2.IsVisible = false;
+                but3.IsVisible = false;
+                but4.IsVisible = false;
+                but5.IsVisible = false;
+                but6.IsVisible = false;
+                but7.IsVisible = false;
+                NameEntry.IsVisible = false;
+                DescriptionEntry.IsVisible = false;
+                CodeEntry.IsVisible = false;
+                SectionEntry.IsVisible = false;
+                ProfessorEntry.IsVisible = false;
+                AddedClasses.IsVisible = false;
+                AddClassButton.IsVisible = false;
+                RemoveClassButton.IsVisible = false;
+                ClearButton.IsVisible = false;
+                CreateButton.IsVisible = false;
+                add1.IsVisible = true;
+                addStartEntry.IsVisible = true;
+                add2.IsVisible= true;
+                addEndEntry.IsVisible = true;
+                add3.IsVisible = true;
+                dayPicker.IsVisible = true;
+                addClearButton.IsVisible = true;
+                addCreateButton.IsVisible = true;
+            }
         }
     }
 }
---
+++ Due It/Due It/Due It/Monad.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Due_It
+{
+    class Monad
+    {
+        /// <summary>
+        /// Holds the time value that the event starts 
+        /// </summary>
+		private int start;
+        /// <summary>
+        /// Holds the time value that the event ends
+        /// </summary>
+		private int end;
+        /// <summary>
+        /// Holds the day of the week it takes place
+        /// </summary>
+        private DayOfWeek date;
+
+
+        public int Start { get { return start; } set {  start = value; } }
+		public int End { get { return end; } set { end = value; } }
+        public DayOfWeek Date { get { return date; } set { date = value; } }
+
+
+        public Monad() { start = 0; end = 0; date = DateTime.Today.DayOfWeek; } 
+        public Monad(int _start, int _end, DayOfWeek _date) { start = _start; end = _end; date = _date; }
+
+
+        public override string ToString() { return $"{Date.ToString("d")} [{TimeCypher(Start)} - {TimeCypher(End)}]"; }
+        public double TimeCypher(int input) { return (input % 4 == 0) ? input / 4 : (input / 4) + ((input % 4)*(0.25)); }
+        public bool IsToday() { return date == DateTime.Today.DayOfWeek; }
+        public bool IsThisWeek() { return date >= DateTime.Today.DayOfWeek && date <= DateTime.Today.AddDays(6).DayOfWeek; }
+
+
+
+    }
+}
